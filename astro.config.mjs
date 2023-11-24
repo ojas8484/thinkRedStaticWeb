fbf2f3f0f16e91466c579f121ebc08bc39783e22
Ojas Aparadh <swaroop@Ojass-MacBook-Air.local>
2023-11-24 12:41:54 +0530

    email functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-
+import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
 	// Enable Svelte to support Svelte components.
 	integrations: [svelte()],
 	site: 'https://ojas8484.github.io',
-	base: '/thinkRedStaticWeb'
+	base: '/thinkRedStaticWeb',
+	output: 'server',
+	
 });
