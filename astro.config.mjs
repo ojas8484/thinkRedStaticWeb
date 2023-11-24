bb3a10a522c3b17cd98516384377281a573e785d
Ojas Aparadh <swaroop@Ojass-MacBook-Air.local>
2023-11-24 14:19:16 +0530

    remove vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 import svelte from '@astrojs/svelte';
 import node from "@astrojs/node";
 // https://astro.build/config
@@ -8,7 +7,5 @@ export default defineConfig({
 	integrations: [svelte()],
 	site: 'https://ojas8484.github.io',
 	base: '/thinkRedStaticWeb',
-	output: 'server',
-	adapter: vercel(),
-	
+	output: 'server'
 });
