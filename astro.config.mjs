c4dca4371a97e4752d4ed4f7736ff9c73226e2dd
Ojas Aparadh <swaroop@Ojass-MacBook-Air.local>
2023-11-24 14:07:27 +0530

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 import svelte from '@astrojs/svelte';
 import node from "@astrojs/node";
 // https://astro.build/config
