b590792953a3333ccb25601d582a1d30424b64d9
Ojas Aparadh <swaroop@Ojass-MacBook-Air.local>
2023-11-27 13:06:03 +0530

    email static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,15 @@
 import { defineConfig } from 'astro/config';
-// import vercel from '@astrojs/vercel/serverless';
 import svelte from '@astrojs/svelte';
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   // Enable Svelte to support Svelte components.
   integrations: [svelte()],
   site: 'https://ojas8484.github.io',
-  base: '/thinkRedStaticWeb'
-  // output: 'server',
-  // adapter: vercel(),
-  ,
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  base: '/thinkRedStaticWeb',
+//   output: "server",
+//   adapter: node({
+//     mode: "standalone"
+//   })
 });
