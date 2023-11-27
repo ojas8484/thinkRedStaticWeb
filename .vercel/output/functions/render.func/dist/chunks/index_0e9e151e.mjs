f463df89bd4b1154634e95f4654615530f80ba4b
Ojas Aparadh <swaroop@Ojass-MacBook-Air.local>
2023-11-27 11:29:56 +0530

    node standalone server

--- .vercel/output/functions/render.func/dist/chunks/index_0e9e151e.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./prerender_ba6dc537.mjs');
-
-export { page };
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,18 @@ import { defineConfig } from 'astro/config';
 // import vercel from '@astrojs/vercel/serverless';
 import svelte from '@astrojs/svelte';
 import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-	// Enable Svelte to support Svelte components.
-	integrations: [svelte()],
-	site: 'https://ojas8484.github.io',
-	base: '/thinkRedStaticWeb',
-	// output: 'server',
-	// adapter: vercel(),
-	
+  // Enable Svelte to support Svelte components.
+  integrations: [svelte()],
+  site: 'https://ojas8484.github.io',
+  base: '/thinkRedStaticWeb'
+  // output: 'server',
+  // adapter: vercel(),
+  ,
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
