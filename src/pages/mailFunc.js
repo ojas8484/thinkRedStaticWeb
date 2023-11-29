a1e8d0d9b744e4547593fb86dd2b8a0eeb418aaa
Ojas Aparadh <swaroop@Ojass-MacBook-Air.local>
2023-11-29 16:39:06 +0530

    email endpoint

--- src/pages/mailFunc.js
+++ src/pages/mailFunc.js
@@ -1,5 +1,5 @@
-export async get(){
-    return new Response (JSON.stringify({'Hello this is a test run'}),{
+export async function get(){
+    return new Response (JSON.stringify({message: 'Hello this is a test run'}),{
         status :200 ,
         headers : {"content-type" : "application/json"},
     });
