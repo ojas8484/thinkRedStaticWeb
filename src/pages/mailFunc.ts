10ef6f25cea8f3a1a216b565308c6a8359d7aeb4
Ojas Aparadh <swaroop@Ojass-MacBook-Air.local>
2023-11-30 14:19:45 +0530

    serverless api

--- src/pages/mailFunc.ts
+++ src/pages/mailFunc.ts
@@ -1,10 +1,20 @@
-import type { APIContext } from "astro";
+// Replace 'https://your-cloudflare-worker.youraccount.workers.dev/your-endpoint' with your Cloudflare Worker URL and endpoint
+const workerURL = 'https://worker-rapid-base-08ea.ojas-443.workers.dev';
 
-export async function get({locals}: APIContext) => {
-  // the type KVNamespace comes from the @cloudflare/workers-types package
-  const { mail_api } = locals.runtime.env;
-
-  return {
-    // ...
-  };
-};
+// Fetch data from Cloudflare Worker
+fetch(workerURL)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Process the data received from Cloudflare Worker
+    console.log('Data received from Cloudflare Worker:', data);
+    // Use the data as needed within your Astro website
+  })
+  .catch(error => {
+    // Handle errors
+    console.error('There was a problem with the fetch operation:', error);
+  });
