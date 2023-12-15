8f64d579075e3ee2a14745a5607477e1fa9407e8
Ojas Aparadh <swaroop@Ojass-MacBook-Air.local>
2023-12-15 18:00:48 +0530

    reverting all changes used for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   // Enable Svelte to support Svelte components.
   integrations: [svelte()],
   site: 'https://ojas8484.github.io',
-  base: '/thinkRedStaticWeb',
-  output: "server",
-  adapter: cloudflare()
+  base: '/thinkRedStaticWeb'
 });
