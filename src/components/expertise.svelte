917035c1ca201a9ad38129996746621794ee9ad3
Ojas Aparadh <swaroop@Ojass-MacBook-Air.local>
2023-12-16 10:04:28 +0530

    minor textual changes

--- src/components/expertise.svelte
+++ src/components/expertise.svelte
@@ -241,7 +241,7 @@
                             <span class=" heading-3-regular fs-3 fs-for-card">{expertiseTitle}</span>
                         </div>
                         <div class="col-md-12 mt d-flex justify-content-center ">
-                            <span class="fs-6">{expertiseData}</span>
+                            <span class="fs-6 footer-fontsize">{expertiseData}</span>
                         </div>
                         <!-- <div class="col-md-12 d-flex justify-content-center ">
                             <span class="body-1-regular  fs-6 ">
